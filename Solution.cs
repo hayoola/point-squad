1574455a394348fe4020cd862f32feed7867e2a2
hayoola <hayoola@gmail.com>
2017-10-26 13:36:29 +0330

    collectDupSegments

--- Solution.cs
+++ Solution.cs
@@ -70,12 +70,18 @@ namespace C1Q1
             CSegment theSegment = null;
 
             // Handling of zero-distance segments
+            //  Move forward until a non-zero-distance found
             if (inArray[inStartIndex] == inArray[inEndIndex])
             {
-                if (inEndIndex < inArray.Length - 1)
+                
+                while (inEndIndex < inArray.Length - 1)
                 {
                     inEndIndex++;
-                    theSegment = new CSegment(inStartIndex, inEndIndex);
+                    if (inArray[inStartIndex] != inArray[inEndIndex])
+                    {
+                        theSegment = new CSegment(inStartIndex, inEndIndex);
+                        break;
+                    }
                 }
                     
             } else
@@ -91,30 +97,13 @@ namespace C1Q1
             EndingPoint = new CPoint(inEndIndex);
         }
 
-        public static bool operator < (CSegment leftSegment, CSegment rightSegment)
-        {
-            return (leftSegment.EndingPoint.Value - leftSegment.StartingPoint.Value) <
-                   (rightSegment.EndingPoint.Value - rightSegment.EndingPoint.Value);
-        }
 
-        public static bool operator > (CSegment leftSegment, CSegment rightSegment)
+        public int Compare(
+            CSegment inSegment)
         {
-            return (leftSegment.EndingPoint.Value - leftSegment.StartingPoint.Value) >
-                   (rightSegment.EndingPoint.Value - rightSegment.EndingPoint.Value);
+            return (EndingPoint.Value - StartingPoint.Value) - 
+                (inSegment.EndingPoint.Value - inSegment.StartingPoint.Value);
         }
-
-        public static bool operator == (CSegment leftSegment, CSegment rightSegment)
-        {
-            return rightSegment != null && (leftSegment != null && (leftSegment.EndingPoint.Value - leftSegment.StartingPoint.Value) ==
-                                            (rightSegment.EndingPoint.Value - rightSegment.EndingPoint.Value));
-        }
-
-        public static bool operator != (CSegment leftSegment, CSegment rightSegment)
-        {
-            return rightSegment != null && (leftSegment != null && (leftSegment.EndingPoint.Value - leftSegment.StartingPoint.Value) !=
-                                            (rightSegment.EndingPoint.Value - rightSegment.EndingPoint.Value));
-        }
-
         
 
         public CPoint StartingPoint { get; }
@@ -134,6 +123,7 @@ namespace C1Q1
         private CSegment[] _minimalSegments;
         private CSegment[] _subMinimalSegments;
         private int _minimalSegmentsSize, _subMinimalSegmentSize;
+        private int _minimalSegmentsDupCount, _mSubMinimalSegmentDupCount;
 
         public CSegmentSquad(
             int[] inArray)
@@ -143,6 +133,8 @@ namespace C1Q1
             _minimalSegmentsSize = 0;
             _subMinimalSegments = new CSegment[4];
             _subMinimalSegmentSize = 0;
+            _minimalSegmentsDupCount = 0;
+            _mSubMinimalSegmentDupCount = 0;
         }
 
         public bool Recruit(
@@ -155,10 +147,45 @@ namespace C1Q1
             CSegment theSegment = CSegment.Create(_mArray, theStartIndex, ref theEndIndex);
 
             if (_minimalSegmentsSize == 0)
+            {
                 _minimalSegments[0] = theSegment;
+                _minimalSegmentsSize++;
+            }
             else
             {
-               
+                int theComparisonResult = theSegment.Compare(_minimalSegments[0]);
+                if (theComparisonResult < 0)
+                {
+                    // theSegment < inArray segment
+
+                    if( _subMinimalSegments[0] == null)
+                        _subMinimalSegments[0] = _minimalSegments[0]; 
+                    else
+                    {
+                        // Oh GOD! we should check for equality here TOO!!
+                        int theSubMinimalCompareResult = _minimalSegments[0].Compare(_subMinimalSegments[0]);
+                        if( theSubMinimalCompareResult < 0 )
+                            _subMinimalSegments[0] = _minimalSegments[0];
+                        else
+                        {
+                            _mSubMinimalSegmentDupCount++;
+                        }
+                    }
+
+
+                    _minimalSegments[0] = theSegment;
+                    _minimalSegmentsDupCount = 0;   // Since we've a new candidate, so reset the dup count
+
+
+                } else if (theComparisonResult == 0)
+                {
+                    // WTF! so we need to mark this equality situation. trigger a variable to show
+                    //  the possible equal values
+                    _minimalSegmentsDupCount++;
+                    // At the end of recruiting process, if this trigger was still set; then
+                    //  we need to collect these segments...
+                    // Look at FinalizeRecruit below
+                }
             }
 
             index += theEndIndex - theStartIndex;   // Segment.create may move forward the end
@@ -166,6 +193,72 @@ namespace C1Q1
                 hasMore = false;
 
             return hasMore;
+        }
+
+        public bool FinalizeRecruit()
+        {
+            bool wasSuccessful = true;
+
+            if (_minimalSegmentsDupCount > 0)
+            {
+                if (_minimalSegmentsDupCount <= 6)
+                {
+                    int theMinimalDistance = _minimalSegments[0].Distance(), theDistance;
+                    for (int i = 0, j = 0; i < _mArray.Length - 1 || j < _minimalSegmentsDupCount; i++)
+                    {
+                        theDistance = _mArray[i + 1] - _mArray[i - 1];
+                        if (theDistance == theMinimalDistance)
+                        {
+                            int theStartIndex = i, theEndIndex = i + 1;
+                            CSegment theSegment = CSegment.Create(_mArray, theStartIndex, ref theEndIndex);
+                            _minimalSegments[_minimalSegmentsSize] = theSegment;
+                            _minimalSegmentsSize++;
+                            j++;
+                        }
+                    }
+                }
+                else
+                {
+                    wasSuccessful = false;
+                }
+            }
+
+            return wasSuccessful;
+        }
+
+        bool collectDupSegments(
+            int inDupCount,
+            CSegment[] inSegmentArray,
+            int inMaxAllowedDup,
+            ref int ioSegmentSize)
+        {
+            bool wasSuccessful = true;
+
+            if (inDupCount > 0)
+            {
+                if (inDupCount <= inMaxAllowedDup)
+                {
+                    int theMinimalDistance = inSegmentArray[0].Distance(), theDistance;
+                    for (int i = 0, j = 0; i < _mArray.Length - 1 || j < inDupCount; i++)
+                    {
+                        theDistance = _mArray[i + 1] - _mArray[i - 1];
+                        if (theDistance == theMinimalDistance)
+                        {
+                            int theStartIndex = i, theEndIndex = i + 1;
+                            CSegment theSegment = CSegment.Create(_mArray, theStartIndex, ref theEndIndex);
+                            inSegmentArray[_minimalSegmentsSize] = theSegment;
+                            ioSegmentSize++;
+                            j++;
+                        }
+                    }
+                }
+                else
+                {
+                    wasSuccessful = false;
+                }
+            }
+
+                return wasSuccessful;
         }
     }
 }
